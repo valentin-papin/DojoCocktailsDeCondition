314d997a66b98063ec5a3772ba8e2287227cd972
Valentin PAPIN <valentin.papin@axa.fr>
2019-04-09 15:20:35 +0200

    feat(MakingCocktail): finds cocktail name

--- CocktailsDeCondition/MakingCocktail.cs
+++ CocktailsDeCondition/MakingCocktail.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Policy;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CocktailsDeCondition
 {
     public class MakingCocktail
     {
+        private static Dictionary<string, List<string>> recipes = new Dictionary<string, List<string>>()
+        {
+            { Cocktails.Beer, new List<string> { Ingredient.Beer }},
+            { Cocktails.Vodka, new List<string> { Ingredient.Vodka }},
+            { Cocktails.Skoll, new List<string> { Ingredient.Beer, Ingredient.Vodka }},
+            { Cocktails.SkollFruitz, new List<string> { Ingredient.Beer, Ingredient.Vodka, Ingredient.Fruit }},
+            { Cocktails.Desperados, new List<string> { Ingredient.Beer, Ingredient.Tequila }},
+            { Cocktails.Monaco, new List<string> { Ingredient.Beer, Ingredient.Fruit, Ingredient.SodaWater }},
+            { Cocktails.Panache, new List<string> { Ingredient.Beer, Ingredient.SodaWater }},
+            { Cocktails.BlueShark, new List<string> { Ingredient.Vodka, Ingredient.Tequila }},
+            { Cocktails.TGV, new List<string> { Ingredient.Vodka, Ingredient.Gin, Ingredient.Tequila }},
+            { Cocktails.LongIsland, new List<string> { Ingredient.Vodka, Ingredient.Tequila, Ingredient.Gin, Ingredient.Fruit }},
+            { Cocktails.VodkaCoffins, new List<string> { Ingredient.Vodka, Ingredient.Gin }},
+            { Cocktails.MarinesVodka, new List<string> { Ingredient.Vodka, Ingredient.Fruit, Ingredient.SodaWater }},
+        };
+
         public string GetNameCocktail(List<string> ingredients)
         {
-            // A vous de jouer
-
-            return "Nom du cocktail, l'abus d'alcool est toléré dans ce dojo";
+            return recipes.FirstOrDefault(x => x.Value.OrderBy(t => t)
+                                                .SequenceEqual(ingredients.OrderBy(t => t)))
+                                                .Key;
         }
     }
 }
